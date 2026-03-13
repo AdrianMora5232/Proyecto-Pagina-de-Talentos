da059cf9ee9755dc3ae67da59b0c868a3df75032
AdrianMora5232 <amorafwdcostarica@gmail.com>
2026-03-13 15:05:15 -0600

    commit

---
+++ src/rutes/Routing.jsx
@@ -0,0 +1,10 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
+function Routing() {
+    <Router>
+        <Routes>
+            <Route path="/" element={<Inicio/>}/>
+        </Routes>
+    </Router>
+}
+export default Routing

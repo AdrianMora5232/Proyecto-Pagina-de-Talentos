2e1951deaf2149a6387c7da544740cde36f979cd
AdrianMora5232 <amorafwdcostarica@gmail.com>
2026-03-16 11:34:11 -0600

    Navbar en la pagina principal

--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Inicio from "../pages/Inicio"
+import Principal from "../pages/Principal"
+
 
 function Routing() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Inicio />} />
-
+                <Route path="/principal" element={<Principal />} />
             </Routes>
         </Router>
     )

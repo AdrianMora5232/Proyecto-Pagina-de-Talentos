213e273832a3f10be05940edd2fff4b6e8717660
AdrianMora5232 <amorafwdcostarica@gmail.com>
2026-03-19 15:43:58 -0600

    Se finalizo la pagina de inicio

--- src/components/InicioPagina.jsx
+++ src/components/InicioPagina.jsx
@@ -53,6 +53,31 @@ function InicioPagina() {
           Nuestra plataforma está diseñada para ayudarte a crecer, ganar visibilidad y convertir tu talento en nuevas oportunidades profesionales desde el primer día.</h4>
       </div>
 
+       {/* Div de cartas */}
+      <div className='FeaturesContainer'>
+        <div className='FeatureCard'>
+          <div className='FeatureIcon'>
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+          </div>
+          <h3 className='FeatureTitle'>Muestra tu trabajo</h3>
+          <p className='FeatureText'>Exhibe tus proyectos con una calidad visual impresionante. Sube tu portafolio de talentos y demuestra tus habilidades al mundo.</p>
+        </div>
+        <div className='FeatureCard'>
+          <div className='FeatureIcon'>
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+          </div>
+          <h3 className='FeatureTitle'>Conecta con expertos</h3>
+          <p className='FeatureText'>Entra en contacto directo con líderes de la industria y clientes potenciales que buscan talento como el tuyo.</p>
+        </div>
+        <div className='FeatureCard'>
+          <div className='FeatureIcon'>
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m21.64 3.64-1.28-1.28a1.21 1.21 0 0 0-1.72 0L2.36 18.64a1.21 1.21 0 0 0 0 1.72l1.28 1.28a1.2 1.2 0 0 0 1.72 0L21.64 5.36a1.2 1.2 0 0 0 0-1.72Z"></path><path d="m14 7 3 3"></path><path d="M5 6v4"></path><path d="M19 14v4"></path><path d="M10 2v2"></path><path d="M7 8H3"></path><path d="M21 16h-4"></path><path d="M11 3H9"></path></svg>
+          </div>
+          <h3 className='FeatureTitle'>Portafolios Inteligentes</h3>
+          <p className='FeatureText'>Crea una presencia online profesional de forma rápida con nuestro constructor intuitivo. Personalización total en clics.</p>
+        </div>
+      </div>
+
 
 
 
